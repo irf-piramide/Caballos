e55df7f49e42ba11f8df7d6b948a62d69fee7e84
ivan <230085@fppiramide.com>
2023-12-15 12:02:34 +0100

    Initial commit

---
+++ Caballos/src/Caballo.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Caballo {
+
+    public String nombre;
+    public boolean vacunado;
+    public double longitud;
+    public double peso;
+    public int edad;
+
+public String getNombre (){
+    return nombre;
+}
+
+
+}
